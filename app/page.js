59c84f7019650a75a1cc5b6e8799861e6ec241d9
Ciara <ciaraduffy24@gmail.com>
2024-09-11 20:03:08 +0100

    Added lost state, replay button

--- app/page.js
+++ app/page.js
@@ -2,11 +2,22 @@
 import Pokemon from "@/components/Pokemon";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import Button from "@/components/Button";
 
 export default function Home() {
   // store data. update when it is changed
   const [data, setData] = useState([]);
   const [counter, setCounter] = useState(0);
+
+  const [lost, setLost] = useState(false);
+
+  function reset() {
+    setCounter(0);
+    setLost(false);
+  
+  }
+ 
+
 
   useEffect(() => {
     generatePokemon()
@@ -34,7 +45,9 @@ export default function Home() {
     }
     else {
       console.log("WRONG!");
-      
+      setLost(true);
+      generatePokemon();
+
     }
   }
 
@@ -79,16 +92,39 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1> which one is heavier?!!!?</h1>
-      <h2>Points:{counter}</h2>
-      <section className="flex md:flex-row flex-col">
-      { 
-        data.map((pokemon, index) => {
-          return <Pokemon name={pokemon.name} image = {pokemon.sprites.front_default} weight = {pokemon.weight} clicked = {click} index = {index} />
-        })
+      { !lost &&
+        <>
+          <h1> which one is heavier?!!!?</h1>
+          <h2>Points:{counter}</h2>
+          <section className="flex md:flex-row flex-col">
+          { 
+            data.map((pokemon, index) => {
+            return <Pokemon name={pokemon.name} image = {pokemon.sprites.front_default} weight = {pokemon.weight} clicked = {click} index = {index} />
+            })
+          }
+          </section>
+        
+        </>
+      
       }
-      </section>
   
+      
+      { lost && 
+      <section>
+        <h1>You Lost!</h1>
+        <h2>Play Again?</h2>
+        {/* no squigglies needed unless function */}
+          <Button text="Yes"
+            onClick={reset}
+            className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          />
+
+            
+          
+      </section>
+      
+      }
+      
 
     </main>
   );
